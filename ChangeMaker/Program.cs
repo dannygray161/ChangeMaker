13eb1aa14809f12dbfb24e8595948239f603526c
Daniel Gray <dieseldan14@gmail.com>
2021-05-27 16:51:34 -0500

    Add project files.

---
+++ ChangeMaker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // an efficient change calculator that does not include half dollars.
+
+            GetChange();
+        }
+        public static void GetChange()
+        {
+            Console.WriteLine("Please enter a dollar amount to receive efficient change: ");
+            double dollarAmount = Convert.ToDouble(Console.ReadLine());
+            
+
+            
+        }
+    }
+}
